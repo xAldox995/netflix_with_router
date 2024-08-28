c4734fc57da2bac35a1f0291a87767f062339cf9
xAldox995 <valzanialdo@gmail.com>
2024-08-28 17:00:03 +0200

    i'm stuck on second fetch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FooterComp from './components/FooterComp';
 import MoviesComp from './components/MoviesComp';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TVShows from './components/TvShows';
+import MovieDetails from './components/MovieDetails';
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
             </>
           } />
           <Route path='/tv-shows' element={<TVShows />}/>
-          <Route path='/movie-details/:movieID' element />
+          <Route path='/movie-details/:movieID' element ={<MovieDetails/>}/>
 
         </Routes>
 
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const MovieDetails = () => {
@@ -12,10 +12,10 @@ const MovieDetails = () => {
     fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=a145b183&s&i=${movieId}`)
       .then((response) => {
         if (response.ok) {
-          console.log("TUTT APPOST", response);
-          return response.json()
+          console.log("Presi dettagli film", response);
+          return response.json();
         } else {
-            throw new Error('Errore nel recupero dettagli del film')
+          throw new Error("Errore nel recupero dettagli del film");
         }
       })
       .then((detailsOfTheFilm) => setMovieDetails(detailsOfTheFilm))
@@ -23,14 +23,32 @@ const MovieDetails = () => {
         console.log("Errore nel catch dei dettagli film", error);
       });
   };
+
+  const fetchComments = () => {
+    fetch(`https://striveschool-api.herokuapp.com/api/comments/${movieId}`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MzAzNjQzYTU2ODAwMTU4ZWMzZDciLCJpYXQiOjE3MjQ4NTUwMjUsImV4cCI6MTcyNjA2NDYyNX0.BWJyG6iboYodhQyxRq4Q41hcvKLmq22XIUUxtU3FwUQ",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Presi commenti Film", response);
+          return response.json();
+        } else {
+          throw new Error("Errore nel recupero commenti del film");
+        }
+      })
+      .then((commentsOfTheFilm) => setComments(commentsOfTheFilm))
+      .catch((err) => {
+        console.log("Errore nel catch dei commenti film", err);
+      });
+  };
+
+  useEffect(() => {
+    fetchMovieDetails();
+    fetchComments();
+  }, [movieId]);
 };
 
-const fetchComments = () => {
-    fetch(`https://striveschool-api.herokuapp.com/api/comments/${movieId}`, {
-        headers: {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MzAzNjQzYTU2ODAwMTU4ZWMzZDciLCJpYXQiOjE3MjQ4NTUwMjUsImV4cCI6MTcyNjA2NDYyNX0.BWJyG6iboYodhQyxRq4Q41hcvKLmq22XIUUxtU3FwUQ"
-        }
-        })
-        ,then(())
-        .catch((err) => {console.log('Errore nel catch dei commenti film', err)})
-}
+export default MovieDetails;
