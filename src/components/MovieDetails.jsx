abc81412816c76d2cc1c4545354f73687cb4686d
xAldox995 <valzanialdo@gmail.com>
2024-08-28 17:34:07 +0200

    finish

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const MovieDetails = () => {
-  const { movieId } = useParams();
-
+  const { movieID } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
-
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const fetchMovieDetails = () => {
-    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=a145b183&s&i=${movieId}`)
+    fetch(`http://www.omdbapi.com/?apikey=66505d5c&i=${movieID}`)
       .then((response) => {
         if (response.ok) {
           console.log("Presi dettagli film", response);
@@ -18,14 +18,19 @@ const MovieDetails = () => {
           throw new Error("Errore nel recupero dettagli del film");
         }
       })
-      .then((detailsOfTheFilm) => setMovieDetails(detailsOfTheFilm))
+      .then((detailsOfTheFilm) => {
+        setMovieDetails(detailsOfTheFilm);
+        console.log(detailsOfTheFilm);
+      })
       .catch((error) => {
         console.log("Errore nel catch dei dettagli film", error);
+        setError("Impossibile caricare i dettagli del film");
+        setIsLoading(false);
       });
   };
 
   const fetchComments = () => {
-    fetch(`https://striveschool-api.herokuapp.com/api/comments/${movieId}`, {
+    fetch(`https://striveschool-api.herokuapp.com/api/comments/${movieID}`, {
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3MzAzNjQzYTU2ODAwMTU4ZWMzZDciLCJpYXQiOjE3MjQ4NTUwMjUsImV4cCI6MTcyNjA2NDYyNX0.BWJyG6iboYodhQyxRq4Q41hcvKLmq22XIUUxtU3FwUQ",
@@ -39,7 +44,10 @@ const MovieDetails = () => {
           throw new Error("Errore nel recupero commenti del film");
         }
       })
-      .then((commentsOfTheFilm) => setComments(commentsOfTheFilm))
+      .then((commentsOfTheFilm) => {
+        console.log(commentsOfTheFilm);
+        setComments(commentsOfTheFilm);
+      })
       .catch((err) => {
         console.log("Errore nel catch dei commenti film", err);
       });
@@ -48,7 +56,37 @@ const MovieDetails = () => {
   useEffect(() => {
     fetchMovieDetails();
     fetchComments();
-  }, [movieId]);
+  }, [movieID]);
+
+  return (
+    <div className="text-light my-5 text-center">
+      {movieDetails && (
+        <>
+          <h1>{movieDetails.Title}</h1>
+          <p>{movieDetails.Plot}</p>
+          <img src={movieDetails.Poster} alt={movieDetails.Title} />
+          <p>Directed by: {movieDetails.Director}</p>
+          <p>Released: {movieDetails.Released}</p>
+          <p>Genre: {movieDetails.Genre}</p>
+        </>
+      )}
+
+      <div className=" comments-section">
+        <h2>Comments</h2>
+        {comments.length > 0 ? (
+          <ul>
+            {comments.map((comment) => (
+              <li key={comment.id}>
+                <strong>{comment.author}:</strong> {comment.text}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No comments available.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetails;
